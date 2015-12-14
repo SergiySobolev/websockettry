b3c77af457c9f963da4edd6ea6ff72c7709c1c53
sobik <sergiy.sobolev@gmail.com>
2015-12-14 23:40:24 +0200

    init

---
+++ src/main/java/com/sbk/websockettry/config/WebConfig.java
@@ -0,0 +1,50 @@
+package com.sbk.websockettry.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.sbk.websockettry.controller")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public WebContentInterceptor webContentInterceptor() {
+        WebContentInterceptor interceptor = new WebContentInterceptor();
+        interceptor.setCacheSeconds(0);
+        interceptor.setUseExpiresHeader(true);
+        interceptor.setUseCacheControlHeader(true);
+        interceptor.setUseCacheControlNoStore(true);
+
+        return interceptor;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/script/**").addResourceLocations("/script/");
+        registry.addResourceHandler("/style/**").addResourceLocations("/style/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(webContentInterceptor());
+    }
+
+}
---
+++ src/main/java/com/sbk/websockettry/controller/ChatController.java
@@ -0,0 +1,28 @@
+package com.sbk.websockettry.controller;
+
+import com.sbk.websockettry.domain.Message;
+import com.sbk.websockettry.domain.OutputMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping("/")
+public class ChatController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String viewApplication() {
+        return "index";
+    }
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/message")
+    public OutputMessage sendMessage(Message message) {
+        return new OutputMessage(message, new Date());
+    }
+}
+
